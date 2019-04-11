c84a713771c5b0ebd00360b312dd25ddece190be
Sam Coco <Samuel.HenriAC@outlook.com>
2019-04-11 23:28:06 +0100

    Queue ADT implemented using list

--- data_structures.py
+++ data_structures.py
@@ -6,7 +6,7 @@ class Stack:
     def push(self, element):
         """Adds an element to the top of the stack and prints the stack.
 
-        :arg
+        args:
             element - The element to be added to stack
         """
         self.stack.append(element)
@@ -15,7 +15,7 @@ class Stack:
     def pop(self):
         """Returns and removes the element at the top of the stack and prints stack.
 
-        :return The element at the top of the stack"""
+        returns: The element at the top of the stack"""
         rear = len(self.stack) - 1
         if not self.is_empty():
             result = self.stack[rear]
@@ -27,7 +27,7 @@ class Stack:
     def is_empty(self):
         """Determines whether stack is empty.
 
-        :returns - True if stack is empty, false if it is not
+        returns: True if stack is empty, false if it is not
         """
         if len(self.stack) == 0:
             print("THIS STACK IS EMPTY")
@@ -42,10 +42,54 @@ class Stack:
     def size(self):
         """Returns the number of element in the stack.
 
-        :returns - The number of elements in stack, or 0 if stack is empty.
+        returns: The number of elements in stack, or 0 if stack is empty.
         """
         if self.is_empty():
             return 0
         else:
             return len(self.stack)
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, element):
+        """Adds an element to the rear of the queue.
+
+        args:
+        element - the element to be added to the back of the queue
+        """
+        self.queue.append(element)
+        self.print()
+
+    def dequeue(self):
+        """Removes and returns the element at the front of the queue."""
+        result = self.queue[0]
+        print("{} REMOVED FROM QUEUE".format(result))
+        del self.queue[0]
+        self.print()
+        return result
+
+    def is_empty(self):
+        """Determines whether the queue is empty (0 elements).
+
+        returns: True if queue has elements, false otherwise"""
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
+
+    def size(self):
+        """Returns the number of elements in the queue.
+
+        returns: The number of elements in the queue
+        """
+        return len(self.queue)
+
+    def print(self):
+        """Displays the queue to the user"""
+        print("QUEUE: {}".format(self.queue))
+
 
