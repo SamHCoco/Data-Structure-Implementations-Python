ea44ec0388743d8178601b52db71bcc8d6479bea
Sam Coco <Samuel.HenriAC@outlook.com>
2019-05-01 23:15:11 +0100

    BST inorder traversal unsuccessfully tested

--- src/test_data_structures.py
+++ src/test_data_structures.py
@@ -4,17 +4,26 @@ from data_structures import BinarySearchTree
 
 class Test(unittest.TestCase):
 
+    def setUp(self):
+        self.values_1 = [7, 11, 6, 8, 20, 1, 9, 14, 22]  # root value 10
+        self.values_2 = [10, 65, -3.14, 12, 43, 256]  # root value 20
+        self.bst_1 = BinarySearchTree(10)
+        self.bst_2 = BinarySearchTree(20)
+        for x in self.values_1:
+            self.bst_1.insert(x)
+        for y in self.values_2:
+            self.bst_2.insert(y)
+
     def test_preorder_traversal(self):
         """Tests preorder traversal of Binary Search Tree."""
-        values_1 = [7, 11, 6, 8, 20, 1, 9, 14, 22]  # root value 10
-        values_2 = [10, 65, -3.14, 12, 43, 256]  # root value 20
         expected_traversal_1 = [10, 7, 6, 1, 8, 9, 11, 20, 14, 22]
         expected_traversal_2 = [20, 10, -3.14, 12, 65, 43, 256]
-        bst_1 = BinarySearchTree(10)
-        bst_2 = BinarySearchTree(20)
-        for x in values_1:
-            bst_1.insert(x)
-        for y in values_2:
-            bst_2.insert(y)
-        self.assertEqual(bst_1.preorder_traversal(), expected_traversal_1)
-        self.assertEqual(bst_2.preorder_traversal(), expected_traversal_2)
+        self.assertEqual(self.bst_1.preorder_traversal(), expected_traversal_1)
+        self.assertEqual(self.bst_2.preorder_traversal(), expected_traversal_2)
+
+    def test_inorder_traversal(self):
+        """Tests inorder traversal for Binary Search Tree."""
+        expected_traversal_1 = [1, 6, 7, 9, 8, 10, 14, 20, 22, 11]
+        expected_traversal_2 = [-6, 10, 12, 20, 43, 65, 256]
+        self.assertEqual(self.bst_1.inorder_traversal(), expected_traversal_1)
+        self.assertEqual(self.bst_2.inorder_traversal(), expected_traversal_2)
